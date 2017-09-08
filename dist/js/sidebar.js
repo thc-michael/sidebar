1658ac1cd4abf69671222d0f00c5006476ff33d5
thc-michael <michael@thehealthconsultancy.com.au>
2017-09-08 10:26:33 +1000

    simplify

--- dist/js/sidebar.js
+++ dist/js/sidebar.js
@@ -11,17 +11,13 @@
         this.$element = $(element);
         this._name = pluginName;
 
-        this.baseSettings = {};
         this.settings = {
             touchEvents: true,
             threshold: 20,
-            backdrop: '.backdrop',
-            panel: '.panel',
-            responsive: []
+            backdrop: this.$element.find('.backdrop'),
+            panel: this.$element.find('.panel'),
+            touchEventsBreakpoint: 768
         };
-
-        this.$backdrop;
-        this.$panel;
 
         this.touchStartX = null;
 
@@ -33,63 +29,20 @@
             var self = this;
 
             // init settings
-            // we need to keep a copy of the user's original settings as all settings can be overridden with responsive design
-            this.baseSettings = $.extend({}, this.settings, this.$element.data(), options);
-            this.refreshSettings();
+            this.settings = $.extend({}, this.settings, this.$element.data(), options);
 
             // touch events
-            this.$panel.on('touchstart', function(e)    { self._onTouchStart(e); });
-            this.$panel.on('touchend', function(e)      { self._onTouchEnd(e); });
-            this.$panel.on('touchmove', function(e)     { self._onTouchMove(e); });
+            this.settings.panel.on('touchstart', function(e)    { self._onTouchStart(e); });
+            this.settings.panel.on('touchend', function(e)      { self._onTouchEnd(e); });
+            this.settings.panel.on('touchmove', function(e)     { self._onTouchMove(e); });
 
             // mouse events
-            this.$panel.mousedown(function(e)           { self._onTouchStart(e); });
-            this.$panel.mouseup(function(e)             { self._onTouchEnd(e); });
-            this.$panel.mousemove(function(e)           { self._onTouchMove(e); });
-            this.$panel.mouseleave(function(e)          { self._onTouchEnd(e); });
+            this.settings.panel.mousedown(function(e)           { self._onTouchStart(e); });
+            this.settings.panel.mouseup(function(e)             { self._onTouchEnd(e); });
+            this.settings.panel.mousemove(function(e)           { self._onTouchMove(e); });
+            this.settings.panel.mouseleave(function(e)          { self._onTouchEnd(e); });
 
-            this.$backdrop.click(function(e)            { self._onBackdropClick(e); });
-
-            $(window).resize(function()                 { self._onWindowResize(); });
-        },
-
-
-        /**
-         * Refreshes settings based on provided breakpoints
-         */
-        refreshSettings: function() {
-            var self = this;
-
-            // reinitialize settings with base settings
-            this.settings = $.extend({}, this.baseSettings);
-
-            this._setElements();
-
-            // override settings based on breakpoints (mobile first)
-            this.settings.responsive.forEach(function(item) {
-                if (window.innerWidth >= item.breakpoint) {
-                    self.settings = $.extend({}, self.settings, item.settings);
-
-                    // elements
-                    self._setElements();
-                }
-            });
-        },
-
-
-        _setElements: function() {
-            this.$backdrop = this.$element.find(this.settings.backdrop);
-            this.$panel = this.$element.find(this.settings.panel);
-        },
-
-
-        /**
-         * Window resize handler
-         */
-        _onWindowResize: function () {
-            if (this.settings.responsive.length) {
-                this.refreshSettings();
-            }
+            this.settings.backdrop.click(function(e)            { self._onBackdropClick(e); });
         },
 
 
@@ -107,7 +60,7 @@
          * @param { touch Event } e
          */
         _onTouchStart: function(e) {
-            if (this.settings.touchEvents) {
+            if (this.settings.touchEvents && window.innerWidth < this.settings.touchEventsBreakpoint) {
                 var xPos = e.touches ? e.touches[0].pageX : e.pageX;
                 this.touchStartX = xPos;
             }
@@ -119,13 +72,13 @@
          * @param { touch Event } e
          */
         _onTouchEnd: function(e) {
-            if (this.settings.touchEvents) {
-                this.$panel.removeClass('no-transition');
+            if (this.settings.touchEvents && window.innerWidth < this.settings.touchEventsBreakpoint) {
+                this.settings.panel.removeClass('no-transition');
 
                 var xPos = e.changedTouches ? e.changedTouches[0].pageX : e.pageX;
 
                 if (this.touchStartX && xPos - this.touchStartX < -this.settings.threshold) {
-                    this.$panel.css('left', '');
+                    this.settings.panel.css('left', '');
                     this.close();
                 }
                 
@@ -139,13 +92,13 @@
          * @param { touch Event } e
          */
         _onTouchMove: function(e) {
-            if (this.settings.touchEvents) {
+            if (this.settings.touchEvents && window.innerWidth < this.settings.touchEventsBreakpoint) {
                 var xPos = e.touches ? e.touches[0].pageX : e.pageX;
 
                 if (this.touchStartX) {
-                    this.$panel.addClass('no-transition');
+                    this.settings.panel.addClass('no-transition');
                     var move = Math.min(0, xPos - this.touchStartX);
-                    this.$panel.css('left', move + 'px');
+                    this.settings.panel.css('left', move + 'px');
                 }
 
             }
@@ -164,7 +117,7 @@
          * Closes sidebar
          */
         close: function() {
-            this.$panel.css('left', '');
+            this.settings.panel.css('left', '');
             this.$element.removeClass('open');
         },
 
